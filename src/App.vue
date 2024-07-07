18ec2b64dc6918e2fe6183125e4ecb57247a7c0a
Shaillydeep <deepshailly68@gmail.com>
2024-07-07 16:13:36 +0530

    Created Vue 3 project using Vite

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <Header />
+  <div> 
+     
+
+  </div>
+
+</template>
+
+<style scoped>
+.app {
+  background-color: #0E1010;
+  color: #0E1010;
+  font-family: Arial, sans-serif;
+}
+
+</style>
