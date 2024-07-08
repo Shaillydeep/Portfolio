4bf690bf00dda3acf9ec7c185a48f064450fffb5
Shaillydeep <deepshailly68@gmail.com>
2024-07-08 17:50:04 +0530

    feat: Add Pill component for displaying styled pills

---
+++ src/components/Pill.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="pill">
+        <span class="pill-text">{{ text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Pill',
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.pill {
+    border-bottom-width: 1px;
+    border-color: #2b2d2d;
+    border-left-width: 1px;
+    border-right-width: 1px;
+    border-style: solid;
+    border-top-width: 1px;
+    align-content: center;
+    align-items: center;
+    background-color: #1d1d1d;
+    border-radius: 30px;
+    display: flex;
+    flex: none;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    gap: 4px;
+    height: min-content;
+    justify-content: center;
+    overflow: visible;
+    padding: 8px 14px;
+    position: relative;
+}
+
+.pill-text {
+    font-size: 16px;
+    color: #fff;
+    font-family: "DM Sans", "DM Sans Placeholder", sans-serif;
+    font-weight: 500;
+    letter-spacing: -.03em;
+    line-height: 1.3em;
+}
+</style>
